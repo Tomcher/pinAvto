f6d7f31604843c378db9b0c2df5ab7729779661b
alexr_cybcube <alexr@cybcube.com>
2024-12-14 04:43:09 +0200

    chore: fuck ai

--- src/features/compileTiresToFile/compileTiresToFile.ts
+++ src/features/compileTiresToFile/compileTiresToFile.ts
@@ -4,10 +4,22 @@ import { parseStringPromise, Builder } from "xml2js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import flexsearch from "flexsearch";
+import { Ollama } from "ollama";
+import { z } from "zod";
+import { zodToJsonSchema } from "zod-to-json-schema";
 
-const index = new flexsearch.Index({
+const ollama = new Ollama({ host: "https://ollama.empireshq.com" });
+
+const SKUSchema = z.object({
+  name: z.string().or(z.null()).describe("Found tire model "),
+  score: z.number()
+});
+
+const makeIndex = new flexsearch.Index({
   tokenize: "full",
-  charset: "extra"
+  charset: "advanced",
+  context: true,
+  resolution: 9,
 });
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -49,6 +61,23 @@ interface Ad {
   };
 }
 
+interface ParsedModels {
+  Shiny: {
+    make: ModelMake[];
+  };
+}
+
+type ModelMake = {
+  $: {
+    name: string;
+  };
+  model: {
+    $: {
+      name: string;
+    };
+  }[];
+};
+
 interface Ads {
   Ads: {
     $: {
@@ -58,6 +87,37 @@ interface Ads {
     Ad: Ad[];
   };
 }
+
+const askAI = async (search: string, list: string[]) => {
+  const jsonSchema = zodToJsonSchema(SKUSchema);
+  // console.log('schema: ', JSON.stringify(jsonSchema))
+  const msg = `
+Do not write any code.
+Do not match characteristics.
+Search for "${search}" in the list and return similarity score and name for the closest match.
+If no match was found or the similarity score is less than 0.9 return {name: null, score: -1}
+Do not return the search term itself, return only the matched item from the list
+list:
+${list.join("\n")}
+If not found return {name: null, score: -1}
+If search term is found return an entry from the list found item in JSON format
+`;
+  // console.log('asking: ', msg)
+  const response = await ollama.chat({
+    model: "codellama:7b-instruct",
+    messages: [
+      {
+        role: "user",
+        content: msg,
+      },
+    ],
+    format: jsonSchema,
+    options: {
+      temperature: 0, // Make responses more deterministic
+    },
+  });
+  return SKUSchema.parse(JSON.parse(response.message.content));
+};
 
 // Define the function to compile <tyre> elements from multiple XML files into one
 export async function compileTiresToFile(
@@ -94,84 +154,190 @@ export async function compileTiresToFile(
       }
     }
 
-    const xmlContent = await fs.readFile(
-      path.join(__dirname, "..", "..", "..", "public", "makes.xml"),
+    const makesXmlContent = await fs.readFile(
+      path.join(__dirname, "..", "..", "..", "settings", "makes.xml"),
+      "utf-8"
+    );
+    const modelsXmlContent = await fs.readFile(
+      path.join(__dirname, "..", "..", "..", "settings", "models.xml"),
       "utf-8"
     );
     const parsedMakes: { BrandValues: { Brand: string[] } } =
-      await parseStringPromise(xmlContent);
-    let i = 0;
-    for (const make of parsedMakes.BrandValues.Brand) {
-      index.add((i += 1), make.trim());
-    }
+      await parseStringPromise(makesXmlContent);
+
+    const parsedModels: ParsedModels =
+      await parseStringPromise(modelsXmlContent);
+
+    const modelIndicies = parsedModels.Shiny.make.reduce<{
+      [key in string]: { ix: flexsearch.Index; store: string[]; make: string };
+    }>((sum, n) => {
+      const ix = new flexsearch.Index({
+        tokenize: "full",
+        charset: "extra",
+        resolution: 9,
+      });
+      sum[n.$.name] = {
+        store: [],
+        ix,
+        make: n.$.name,
+      };
+      n.model.forEach((m, i) => {
+        const name = m.$.name;
+        ix.add(i, name);
+        sum[n.$.name].store.push(name);
+      });
+      return sum;
+    }, {});
+
+    const makes = parsedMakes.BrandValues.Brand.map((it: string) => it.trim());
+    makes.forEach((make, i) => {
+      makeIndex.add(i, make);
+    });
+
     // Add UUID to each tire
     allTires = allTires.map((tire) => ({ ...tire }));
+
+    const searchMake = (make: string) => {
+      let found = makeIndex.search(make);
+      if (!found || !found.length) {
+        const split = make.split(" ");
+        if (split.length > 0) {
+          for (const chunk of split) {
+            found = makeIndex.search(chunk);
+            if (found && found.length) {
+              return makes[found[0] as number];
+            }
+          }
+        }
+        return make;
+      } else {
+        return makes[found[0] as number];
+      }
+    };
+
+    const searchModel = async (make: string, model: string) => {
+      const foundIx = modelIndicies[make];
+      if (foundIx) {
+        const { ix, store } = foundIx;
+        const simpleSearch = store.find((s) => s === model);
+        if (simpleSearch) {
+          console.log("simple: ", model);
+          return simpleSearch;
+        }
+        const found = ix.search(model);
+        if (found && found.length) {
+          console.log("found: ", store[found[0] as number], " : ", model);
+          return store[found[0] as number];
+        }
+        const split = model.split(" ");
+        if (split.length > 0) {
+          console.log("  partial search: ", model);
+          let cursor = 0;
+          while (cursor < split.length - 2) {
+            const part = split.slice((cursor += 1));
+            console.log("  seaching for :", part.join(" "));
+            const fp = ix.search(part.join(" "));
+            if (fp && fp.length) {
+              console.log(
+                "  partial found: ",
+                store[fp[0] as number],
+                " : ",
+                model
+              );
+              return store[fp[0] as number];
+            }
+          }
+        }
+        console.log("+++search with AI");
+        const resp = await askAI(model, store);
+        if (resp.name) {
+          console.log("AI FOUND!!!: ", resp, " : ", model, store, make);
+          // if (resp.distance )
+          return resp.name
+        } else {
+          console.log("AI NOT FOUND: ", resp, model, store)
+        }
+        
+      }
+
+      console.log("not found: ", model, " : ", make);
+      return model;
+    };
+    const seenIds = new Set<string>(); // To store unique product IDs
+    const filtered = allTires.filter((tire: Tire) => {
+      if (seenIds.has(tire.product_id[0])) {
+        return false; // Remove the duplicate by skipping it
+      }
+      seenIds.add(tire.product_id[0]); // Add the ID to the set
+      return true; // Keep the unique Ad
+    });
+
+    const adsContent: Ad[] = [];
+    for (const tire of filtered) {
+      // const model =
+      //   tire.brand[0] === "Tigar" && tire.model[0] === "HP"
+      //     ? "High Performance"
+      //     : tire.model[0].replace(/CF-(\d+)/, "CF$1");
+      let brandName;
+      switch (tire.brand[0].toLowerCase()) {
+        case "belshina":
+          brandName = "Белшина"
+          break;
+        default:
+          brandName = tire.brand[0]
+      }
+      let brand = searchMake(brandName);
+      if (tire.model[0].toLowerCase().indexOf("viatti") > -1) {
+        brand = searchMake("Viatti")
+      }
+      const model = await searchModel(brand, tire.model[0]);
+      adsContent.push({
+        Id: tire.product_id[0],
+        Address: "Ставропольский край, Ставрополь, Шпаковская ул., 115",
+        Category: "Запчасти и аксессуары",
+        Description: `⭐ ⭐ ⭐ ⭐ ⭐ \nЛучшая ${tire.brand[0]} ${tire.size[0]} ${model} Арт. ${tire.artikul[0]} купить в Ставрополе ${tire.season[0]} ${tire.thorn[0]}`,
+        GoodsType: "Шины, диски и колёса",
+        AdType: "Товар от производителя",
+        ProductType: "Легковые шины",
+        Brand: brand,
+        Model: model,
+        TireSectionWidth: tire.width[0],
+        RimDiameter: tire.diameter[0].match(/\d+/g)?.join("") || "",
+        TireAspectRatio: tire.height[0],
+        TireType: (() => {
+          if (tire.thorn[0] === "Шипованная") {
+            return "Зимние шипованные";
+          } else if (tire.thorn[0] === "Нешипованная") {
+            return "Зимние нешипованные";
+          } else {
+            switch (tire.season[0]) {
+              case "Всесезонная":
+                return "Всесезонные";
+              case "Летняя":
+                return "Летние";
+              default:
+                return tire.season[0];
+            }
+          }
+        })(),
+        Quantity: "за 1 шт.",
+        Condition: "Новое",
+        Images: {
+          Image: {
+            $: {
+              url: `https://b2b.pin-avto.ru/public/photos/format/${tire.product_id}.jpeg`,
+            },
+          },
+        },
+      });
+    }
 
     // Create the new XML structure based on the Sample template
     // Create the new XML structure based on the Sample template
     const ads: Ads = {
       Ads: {
         $: { formatVersion: "3", target: "Avito.ru" },
-        Ad: (() => {
-          const seenIds = new Set<string>(); // To store unique product IDs
-          const filtered = allTires.filter((tire: Tire) => {
-            if (seenIds.has(tire.product_id[0])) {
-              return false; // Remove the duplicate by skipping it
-            }
-            seenIds.add(tire.product_id[0]); // Add the ID to the set
-            return true; // Keep the unique Ad
-          });
-          return filtered.map(async (tire: Tire) => {
-            const model =
-              tire.brand[0] === "Tigar" && tire.model[0] === "HP"
-                ? "High Performance"
-                : tire.model[0].replace(/CF-(\d+)/, "CF$1");
-            const make = await index.search(tire.brand[0])
-            if (!make || !make.length) {
-              console.log("not found:", make)
-            }
-            
-            return {
-              Id: tire.product_id[0],
-              Address: "Ставропольский край, Ставрополь, Шпаковская ул., 115",
-              Category: "Запчасти и аксессуары",
-              Description: `⭐ ⭐ ⭐ ⭐ ⭐ \nЛучшая ${tire.brand[0]} ${tire.size[0]} ${model} Арт. ${tire.artikul[0]} купить в Ставрополе ${tire.season[0]} ${tire.thorn[0]}`,
-              GoodsType: "Шины, диски и колёса",
-              AdType: "Товар от производителя",
-              ProductType: "Легковые шины",
-              Brand: tire.brand[0],
-              Model: model,
-              TireSectionWidth: tire.width[0],
-              RimDiameter: tire.diameter[0].match(/\d+/g)?.join("") || "",
-              TireAspectRatio: tire.height[0],
-              TireType: (() => {
-                if (tire.thorn[0] === "Шипованная") {
-                  return "Зимние шипованные";
-                } else if (tire.thorn[0] === "Нешипованная") {
-                  return "Зимние нешипованные";
-                } else {
-                  switch (tire.season[0]) {
-                    case "Всесезонная":
-                      return "Всесезонные";
-                    case "Летняя":
-                      return "Летние";
-                    default:
-                      return tire.season[0];
-                  }
-                }
-              })(),
-              Quantity: "за 1 шт.",
-              Condition: "Новое",
-              Images: {
-                Image: {
-                  $: {
-                    url: `https://b2b.pin-avto.ru/public/photos/format/${tire.product_id}.jpeg`,
-                  },
-                },
-              },
-            };
-          });
-        })(),
+        Ad: adsContent,
       },
     };
 
